4a1d529b00b4d5d709d78bd009c2228dec69153d
Jakub Kowalik <j.kowalik0928@gmail.com>
2023-11-14 13:52:08 +0100

    horizontal scroll

---
+++ app/src/main/java/com/example/olinestore/Fragments/ItemFragment.java
@@ -0,0 +1,45 @@
+package com.example.olinestore.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.olinestore.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ItemFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ItemFragment extends Fragment {
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
