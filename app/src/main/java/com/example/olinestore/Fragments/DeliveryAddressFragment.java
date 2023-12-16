dae983acd097c6ac9a564c0fa045c04855c84a69
Filip Szczepanek <szczepanek_filip@wp.pl>
2023-12-16 18:31:55 +0100

    User fragment done

---
+++ app/src/main/java/com/example/olinestore/Fragments/DeliveryAddressFragment.java
@@ -0,0 +1,117 @@
+package com.example.olinestore.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.olinestore.R;
+import com.example.olinestore.UserPanelActivity;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.StringJoiner;
+
+
+public class DeliveryAddressFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_delivery_address, container,
+                                false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        setCancellButtonAction();
+        setApplyButtonFunction();
+
+    }
+
+    private void init(@NonNull View view) {
+        firestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        cancellButton = view.findViewById(R.id.CancelButton);
+        applyButton = view.findViewById(R.id.ApplyButton);
+        streetInput = view.findViewById(R.id.StreetAndNumber);
+        postalCodeInput = view.findViewById(R.id.PostalCode);
+        cityInput = view.findViewById(R.id.City);
+
+    }
+
+    private void setCancellButtonAction() {
+        cancellButton.setOnClickListener(l -> {
+
+            clearInputs();
+            ((UserPanelActivity) getActivity()).switchDeliveryAddresForm();
+        });
+    }
+
+    private void clearInputs() {
+        streetInput.getEditText().setText("");
+        postalCodeInput.getEditText().setText("");
+        cityInput.getEditText().setText("");
+    }
+
+    private void setApplyButtonFunction() {
+        applyButton.setOnClickListener(l -> {
+            setUserAddress(firebaseAuth.getUid());
+        });
+    }
+
+    private void setUserAddress(String Uid) {
+        StringJoiner joiner = new StringJoiner(",");
+        joiner.add(streetInput.getEditText().getText())
+                .add(postalCodeInput.getEditText().getText())
+                .add(cityInput.getEditText().getText());
+        String newAddress = joiner.toString();
+        firestore
+                .collection("registeredUsers")
+                .document(Uid)
+                .update("address", newAddress)
+                .addOnCompleteListener(
+                        task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(),
+                                               "Delivery address successfully updated",
+                                               Toast.LENGTH_SHORT)
+                                        .show();
+                                clearInputs();
+                                ((UserPanelActivity) getActivity()).switchDeliveryAddresForm();
+                            }
+
+                        });
+    }
+
+
+    private FirebaseFirestore firestore;
+    private FirebaseAuth firebaseAuth;
+
+    private Button cancellButton;
+
+    private Button applyButton;
+    private TextInputLayout streetInput;
+    private TextInputLayout postalCodeInput;
+    private TextInputLayout cityInput;
+}
--- app/src/main/java/com/example/olinestore/UserPanelActivity.java
+++ app/src/main/java/com/example/olinestore/UserPanelActivity.java
@@ -3,11 +3,17 @@ package com.example.olinestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UserPanelActivity extends AppCompatActivity {
 
@@ -18,13 +24,24 @@ public class UserPanelActivity extends AppCompatActivity {
 
         setUpBackButtonOnClick();
         setUpLogOutButtonOnClickInfo();
+
+        changeAddressButton.setOnClickListener(l -> {
+            switchDeliveryAddresForm();
+        });
+        updateUserAddress();
     }
 
     private void initialize() {
         setContentView(R.layout.activity_user_panel);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
         logOutButton = (ImageView) findViewById(R.id.logOutButton);
         backButton = findViewById(R.id.backButton);
-        firebaseAuth = FirebaseAuth.getInstance();
+        addressInfoTV = findViewById(R.id.textInfo);
+        currentAddressTV = findViewById(R.id.CurrentDeliveryAddress);
+        changeAddressButton = findViewById(R.id.changeButton);
+        deliveryFragment = findViewById(R.id.fragmentContainerView3);
+
     }
 
     private void setUpBackButtonOnClick() {
@@ -41,7 +58,59 @@ public class UserPanelActivity extends AppCompatActivity {
         });
     }
 
+    public void switchDeliveryAddresForm() {
+        if (!isFormShowed) {
+
+            currentAddressTV.setVisibility(View.INVISIBLE);
+            changeAddressButton.setVisibility(View.INVISIBLE);
+            addressInfoTV.setVisibility(View.INVISIBLE);
+            deliveryFragment.setVisibility(View.VISIBLE);
+            isFormShowed = true;
+        } else {
+            updateUserAddress();
+            currentAddressTV.setVisibility(View.VISIBLE);
+            changeAddressButton.setVisibility(View.VISIBLE);
+            addressInfoTV.setVisibility(View.VISIBLE);
+            deliveryFragment.setVisibility(View.INVISIBLE);
+            isFormShowed = false;
+        }
+    }
+
+    private void updateUserAddress() {
+        firestore
+                .collection("registeredUsers")
+                .document(firebaseAuth.getUid())
+                .get()
+                .addOnCompleteListener(
+                        task ->
+                        {
+                            if (task.isSuccessful()) {
+
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    String address =
+                                            document.getString("address");
+                                    if (address == null || address.isEmpty()) {
+                                        currentAddressTV.setText("Please add delivery address");
+                                    } else {
+                                        currentAddressTV.setText(address);
+                                    }
+                                }
+                            }
+                        });
+
+    }
+
+    private FirebaseFirestore firestore;
+    private boolean isFormShowed = false;
+
     private ImageView logOutButton;
     private FirebaseAuth firebaseAuth;
     private ImageButton backButton;
+
+    private View deliveryFragment;
+
+    private Button changeAddressButton;
+    private TextView addressInfoTV;
+    private TextView currentAddressTV;
 }
